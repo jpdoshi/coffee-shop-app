93f9a5698ca142b9c630ac565444eeace9700750
jpdoshi <jpdoshi2811@gmail.com>
2025-03-23 19:55:36 +0530

    fix: modal on tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,111 +6,115 @@ import HomeIcon from "@/components/Icons/HomeIcon";
 import BellIcon from "@/components/Icons/BellIcon";
 import HeartIcon from "@/components/Icons/HeartIcon";
 import BagIcon from "@/components/Icons/BagIcon";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 
 const Layout = () => {
   return (
-    <>
-      <Tabs
-        screenOptions={{
-          tabBarStyle: {
-            marginBottom:
-              Dimensions.get("screen").height -
-              Dimensions.get("window").height -
-              (StatusBar.currentHeight || 0),
-            height: 72,
-            shadowColor: "transparent",
-            borderColor: "white",
-            position: "absolute",
-            borderTopLeftRadius: 20,
-            borderTopRightRadius: 20,
-          },
-          tabBarShowLabel: false,
-          tabBarItemStyle: {
-            flexDirection: "row",
-            alignItems: "center",
-            backgroundColor: "transparent",
-            height: 72,
-          },
-        }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            headerShown: false,
-            title: "Home",
-            tabBarIcon: ({ focused }) => (
-              <View className="flex-col items-center">
-                <View className="h-[28px]">
-                  <HomeIcon size={22} focused={focused} />
-                </View>
-                <View
-                  className={`h-1 w-3 bg-active rounded-full ${
-                    focused ? "opacity-100" : "opacity-0"
-                  }`}
-                />
-              </View>
-            ),
+    <GestureHandlerRootView>
+      <BottomSheetModalProvider>
+        <Tabs
+          screenOptions={{
+            tabBarStyle: {
+              marginBottom:
+                Dimensions.get("screen").height -
+                Dimensions.get("window").height -
+                (StatusBar.currentHeight || 0),
+              height: 72,
+              shadowColor: "transparent",
+              borderColor: "white",
+              position: "absolute",
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+            },
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+              flexDirection: "row",
+              alignItems: "center",
+              backgroundColor: "transparent",
+              height: 72,
+            },
           }}
-        />
-        <Tabs.Screen
-          name="liked"
-          options={{
-            headerShown: false,
-            title: "Liked",
-            tabBarIcon: ({ focused }) => (
-              <View className="flex-col items-center">
-                <View className="h-[28px]">
-                  <HeartIcon size={24} focused={focused} />
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              headerShown: false,
+              title: "Home",
+              tabBarIcon: ({ focused }) => (
+                <View className="flex-col items-center">
+                  <View className="h-[28px]">
+                    <HomeIcon size={22} focused={focused} />
+                  </View>
+                  <View
+                    className={`h-1 w-3 bg-active rounded-full ${
+                      focused ? "opacity-100" : "opacity-0"
+                    }`}
+                  />
                 </View>
-                <View
-                  className={`h-1 w-3 bg-active rounded-full ${
-                    focused ? "opacity-100" : "opacity-0"
-                  }`}
-                />
-              </View>
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="cart"
-          options={{
-            headerShown: false,
-            title: "Cart",
-            tabBarIcon: ({ focused }) => (
-              <View className="flex-col items-center">
-                <View className="h-[28px]">
-                  <BagIcon size={24} focused={focused} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="liked"
+            options={{
+              headerShown: false,
+              title: "Liked",
+              tabBarIcon: ({ focused }) => (
+                <View className="flex-col items-center">
+                  <View className="h-[28px]">
+                    <HeartIcon size={24} focused={focused} />
+                  </View>
+                  <View
+                    className={`h-1 w-3 bg-active rounded-full ${
+                      focused ? "opacity-100" : "opacity-0"
+                    }`}
+                  />
                 </View>
-                <View
-                  className={`h-1 w-3 bg-active rounded-full ${
-                    focused ? "opacity-100" : "opacity-0"
-                  }`}
-                />
-              </View>
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="notification"
-          options={{
-            headerShown: false,
-            title: "Notifications",
-            tabBarIcon: ({ focused }) => (
-              <View className="flex-col items-center">
-                <View className="h-[28px]">
-                  <BellIcon size={24} focused={focused} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="cart"
+            options={{
+              headerShown: false,
+              title: "Cart",
+              tabBarIcon: ({ focused }) => (
+                <View className="flex-col items-center">
+                  <View className="h-[28px]">
+                    <BagIcon size={24} focused={focused} />
+                  </View>
+                  <View
+                    className={`h-1 w-3 bg-active rounded-full ${
+                      focused ? "opacity-100" : "opacity-0"
+                    }`}
+                  />
                 </View>
-                <View
-                  className={`h-1 w-3 bg-active rounded-full ${
-                    focused ? "opacity-100" : "opacity-0"
-                  }`}
-                />
-              </View>
-            ),
-          }}
-        />
-      </Tabs>
-    </>
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="notification"
+            options={{
+              headerShown: false,
+              title: "Notifications",
+              tabBarIcon: ({ focused }) => (
+                <View className="flex-col items-center">
+                  <View className="h-[28px]">
+                    <BellIcon size={24} focused={focused} />
+                  </View>
+                  <View
+                    className={`h-1 w-3 bg-active rounded-full ${
+                      focused ? "opacity-100" : "opacity-0"
+                    }`}
+                  />
+                </View>
+              ),
+            }}
+          />
+        </Tabs>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 };
 
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,9 @@
-import { useCallback, useRef } from "react";
+import React, { useCallback, useRef } from "react";
 import { Pressable, Text, TextInput, View } from "react-native";
 import Svg, { G, Path } from "react-native-svg";
 
 import ScreenView from "@/components/ScreenView";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import {
-  BottomSheetModal,
-  BottomSheetModalProvider,
-  BottomSheetView,
-} from "@gorhom/bottom-sheet";
+import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
 
 const SearchBar = () => (
   <View className="bg-white rounded-xl px-3 flex-row items-center py-1.5 gap-1 shadow">
@@ -40,44 +35,51 @@ const index = () => {
   const modalRef = useRef(null);
 
   const showModal = useCallback(() => {
+    // @ts-ignore
     modalRef.current?.present();
   }, []);
 
   return (
-    <GestureHandlerRootView className="flex-1">
-      <BottomSheetModalProvider>
-        <ScreenView>
-          {/* top section */}
-          <View className="py-6">
-            <Text className="text-tertiary">Location</Text>
-            <Pressable
-              className="flex-row items-center mb-4"
-              onPress={showModal}
-            >
-              <Text className="font-semibold text-lg">Rajkot, Gujarat</Text>
-              <Svg width="20px" height="20px" viewBox="0 0 24 24" fill="none">
-                <G id="SVGRepo_iconCarrier">
-                  <Path
-                    d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z"
-                    fill="#000000"
-                  />
-                </G>
-              </Svg>
-            </Pressable>
-            <SearchBar />
-          </View>
+    <>
+      <ScreenView>
+        {/* top section */}
+        <View className="py-6">
+          <Text className="text-tertiary">Location</Text>
+          <Pressable className="flex-row items-center mb-4" onPress={showModal}>
+            <Text className="font-semibold text-lg">Rajkot, Gujarat</Text>
+            <Svg width="20px" height="20px" viewBox="0 0 24 24" fill="none">
+              <G id="SVGRepo_iconCarrier">
+                <Path
+                  d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z"
+                  fill="#000000"
+                />
+              </G>
+            </Svg>
+          </Pressable>
+          <SearchBar />
+        </View>
 
-          {/* categories */}
+        {/* categories */}
 
-          {/* cards list */}
-        </ScreenView>
-        <BottomSheetModal ref={modalRef}>
-          <BottomSheetView className="items-center h-[50vh]">
-            <Text>Show Cities List</Text>
-          </BottomSheetView>
-        </BottomSheetModal>
-      </BottomSheetModalProvider>
-    </GestureHandlerRootView>
+        {/* cards list */}
+      </ScreenView>
+      <BottomSheetModal
+        ref={modalRef}
+        backdropComponent={() => (
+          <Pressable
+            className="flex-1 bg-[rgba(0,0,0,0.1)] duration-300 absolute top-0 left-0 w-full h-full"
+            onPress={() => {
+              // @ts-ignore
+              modalRef.current?.close();
+            }}
+          />
+        )}
+      >
+        <BottomSheetView className="items-center h-[50vh]">
+          <Text>Show Cities List</Text>
+        </BottomSheetView>
+      </BottomSheetModal>
+    </>
   );
 };
 
