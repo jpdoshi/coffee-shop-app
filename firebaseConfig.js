26e4ad782ab137689c1234f4b0a3e75391e2a2e7
jpdoshi <jpdoshi2811@gmail.com>
2025-03-28 14:18:22 +0530

    deps: install firebase SDK

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAl51zgbTSTszWpSRhN9qTbjVG4M6YJaZU",
+  authDomain: "coffee-app-c5d95.firebaseapp.com",
+  projectId: "coffee-app-c5d95",
+  storageBucket: "coffee-app-c5d95.firebasestorage.app",
+  messagingSenderId: "490540523655",
+  appId: "1:490540523655:web:d19f2baad4a459d9739822",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
